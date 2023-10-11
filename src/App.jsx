c45021c296c692cdce07b7abd1e7801d3264b243
Tanish Kushwaha <tanishkushwahatk@gmail.com>
2023-10-11 13:19:59 +0530

    Added loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import './App.css'
 const App = () => {
 
     const [url, setUrl] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const nextActivity = () => {
-        
+        setLoading(true);
         fetch('https://www.boredapi.com/api/activity')
         .then(res => res.json())
         .then(data => {
-
+            setLoading(false);
             console.log(data);
             setUrl(data.activity);
         })
@@ -28,8 +29,20 @@ const App = () => {
     }, []);
 
     return (
-        <>
-            <span key={url}>{url}</span>
+        <>  
+            {loading ? (
+                <div className="loader-container">
+                    <div className="loadingio-spinner-eclipse-kganed8ra7">
+                        <div className="ldio-d8rv4bk8fds">
+                            <div></div>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <div className="activity-container">
+                <span key={url}>{url}</span>    
+            </div>
+            )}
         </>
     );
 }
